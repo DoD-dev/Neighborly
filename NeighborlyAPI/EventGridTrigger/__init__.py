0c0c7f6e02ecfcb9442a3205aaf5e734896bd545
DoD-dev <cautapchung98@gmail.com>
2023-10-17 14:37:12 +0700

    update

--- NeighborlyAPI/EventGridTrigger/__init__.py
+++ NeighborlyAPI/EventGridTrigger/__init__.py
@@ -4,13 +4,4 @@ import logging
 import azure.functions as func
 
 def main(event: func.EventGridEvent):
-
-    result = json.dumps({
-        'id': event.id,
-        'data': event.get_json(),
-        'topic': event.topic,
-        'subject': event.subject,
-        'event_type': event.event_type,
-    })
-
-    logging.info('Python EventGrid trigger processed an event:')
+    logging.info('Python EventGrid trigger processed an event')
